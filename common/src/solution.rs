c784cc4f38cb59a6a71ae32ffbf738d40450c7f0
Connor Macdonald <kiwiconnor@proton.me>
2025-12-03 11:40:00 +1300

    Added input parsing profiling

--- common/src/solution.rs
+++ common/src/solution.rs
@@ -2,8 +2,22 @@ use std::fmt::Display;
 use std::any::type_name;
 use log::info;
 
-use crate::{Input, InputParser};
+use crate::{
+    Input, 
+    InputParser
+};
 
+fn run_timed<F, T>(name: &str, method: F) -> T
+    where F: FnOnce() -> T 
+{
+    let timer = std::time::Instant::now();
+    let result = method();
+    let duration = timer.elapsed();
+
+    log::info!("{name} {duration:?}");
+
+    return result;
+}
 
 pub trait ProblemQuestion: Sized {
     type Parser: InputParser;
@@ -12,15 +26,24 @@ pub trait ProblemQuestion: Sized {
     fn solve<S: Solution<Self>>(input: Input) -> Self::Output {
         let solution_name = type_name::<S>();
         let problem_name = type_name::<Self>();
-        info!("Running {solution_name} for {problem_name}");
+        info!("Solving {problem_name} with {solution_name}");
 
-        let timer = std::time::Instant::now();
-        let result  = S::answer(input.parse::<Self::Parser>());
-        let duration = timer.elapsed();
-        
-        info!("Solution Result: {result}");
-        info!("Duration: {duration:?}");
+        let result = run_timed(
+            "Total:             ", 
+            || {
+                let parsed_input = run_timed(
+                    "Parse Input:       ", 
+                    || input.parse::<Self::Parser>()
+                );
 
+                run_timed(
+                    "Calculating Answer:", 
+                    || S::answer(parsed_input)
+                )
+            }
+        );
+
+        info!("Solution Result: {result}\n");
         result
     }
 }
