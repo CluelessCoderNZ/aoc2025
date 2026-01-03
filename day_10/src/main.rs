e52b32d103e875258a28258965ca2a99a0da02c2
Connor Macdonald <kiwiconnor@proton.me>
2026-01-03 21:26:34 +1300

    Day Ten Problem Two Solution

--- day_10/src/main.rs
+++ day_10/src/main.rs
@@ -14,6 +14,12 @@ impl ProblemQuestion for ProblemOne {
     type Output = u64;
 }
 
+struct ProblemTwo;
+impl ProblemQuestion for ProblemTwo {
+    type Parser = MachineManualParser;
+    type Output = u64;
+}
+
 struct DayTenSolution;
 
 impl Solution<ProblemOne> for DayTenSolution {
@@ -25,19 +31,35 @@ impl Solution<ProblemOne> for DayTenSolution {
     }
 }
 
+impl Solution<ProblemTwo> for DayTenSolution {
+    fn answer(input: Vec<Machine>) -> u64 {
+        input
+            .into_iter()
+            .map(|machine| machine.min_joltage_presses())
+            .sum()
+    }
+}
+
 fn main() {
     ProblemOne::solve::<DayTenSolution>(TEST_INPUT);
+    ProblemTwo::solve::<DayTenSolution>(TEST_INPUT);
 }
 
 #[cfg(test)]
 mod test {
     use common::ProblemQuestion;
 
-    use crate::{DayTenSolution, ProblemOne, TEST_EXAMPLE};
+    use crate::{DayTenSolution, ProblemOne, ProblemTwo, TEST_EXAMPLE};
 
     #[test]
     fn test_problem_one_example() {
         let result = ProblemOne::solve::<DayTenSolution>(TEST_EXAMPLE);
         assert_eq!(result, 7)
+    }
+
+    #[test]
+    fn test_problem_two_example() {
+        let result = ProblemTwo::solve::<DayTenSolution>(TEST_EXAMPLE);
+        assert_eq!(result, 33)
     }
 }
--- day_10/src/model.rs
+++ day_10/src/model.rs
@@ -1,7 +1,9 @@
 use std::{collections::VecDeque, fmt::Display};
+use microlp::{ComparisonOp, OptimizationDirection, Problem, Variable};
 
 pub type IndicatorState = u16;
 
+#[derive(Clone)]
 pub struct Button {
     pub toggle_pattern: Vec<usize>,
 }
@@ -15,6 +17,12 @@ impl Button {
         }
 
         mask
+    }
+
+    fn add_joltage(&self, joltage: &mut Vec<u64>, presses: u64) {
+        for index in &self.toggle_pattern {
+            joltage[*index] += presses;
+        }
     }
 }
 
@@ -66,6 +74,60 @@ impl Machine {
         }
 
         unreachable!()
+    }
+}
+
+impl Machine {
+    pub fn min_joltage_presses(&self) -> u64 {
+        let solution = self.get_min_joltage_solution();
+        self.test_joltage_solution(&solution).expect("Solution is valid");
+        
+        solution.iter().sum()
+    }
+
+    fn test_joltage_solution(&self, solution: &Vec<u64>) -> Result<(), Vec<u64>> {
+        let mut joltages = vec![0u64; self.joltage_req.len()];
+
+        // Add button presses to joltage
+        for (index, button_presses) in solution.iter().enumerate() {
+            self.button_wiring[index].add_joltage(&mut joltages, *button_presses);
+        }
+
+        // Check if meets requirements
+        if self.joltage_req == joltages {
+            return Ok(());
+        } else {
+            return Err(joltages);
+        }
+    }
+
+    /// Returns presses for each button in order to get min solution
+    fn get_min_joltage_solution(&self) -> Vec<u64> {
+        let (problem, vars) = self.form_sys_equations_problem();
+        let solution = problem.solve().expect("All machines solveable");
+        
+        vars.into_iter()
+            .map(|(_, var)| solution.var_value_rounded(var) as u64)
+            .collect()
+    }
+
+    fn form_sys_equations_problem(&self) -> (Problem, Vec<(Button, Variable)>) {
+        // Minimise system of equations where each button press is an independent variable
+        let mut problem = Problem::new(OptimizationDirection::Minimize);
+        let button_vars: Vec<_> = self.button_wiring.iter()
+            .map(|button| (button.clone(), problem.add_integer_var(1.0, (0, i32::MAX))))
+            .collect();
+
+        // Constraints formed from sum of wired button presses for each joltage req
+        for (index, joltage) in self.joltage_req.iter().enumerate() {
+            let wired_variables = button_vars.iter()
+                .filter(|(button, _)| button.toggle_pattern.contains(&index))
+                .map(|(_, var)| (var.clone(), 1.0));
+            
+            problem.add_constraint(wired_variables, ComparisonOp::Eq, *joltage as f64);
+        }
+
+        (problem, button_vars)
     }
 }
 
